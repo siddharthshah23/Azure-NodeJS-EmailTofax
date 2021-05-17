76b0b97a9bebd3880fd61d520fd83880e14b0b0c
Siddharth Shah <62000981+siddharthshah23@users.noreply.github.com>
2021-05-17 11:29:11 -0400

    ngrox added

--- Function_SendFax/index.js
+++ Function_SendFax/index.js
@@ -1,19 +1,10 @@
-const axios = require("axios");
-const qs = require("qs");
-const token = "KEY017880D69A68B843E7E805E3C1634345_meOtfQUgtxMZdh8X8wc3Vl";
-
 module.exports = async function (context, req) {
   // Getting Values from HTTP Reqquest
 
-  const content = req.body.content;
-
-  //   const parsedOutput = JSON.parse(content);
-  const mediaurl = `Hi`;
-  //   const faxto = content.faxto;
+  const responseMessage = req.body.data.payload.status;
 
   context.log("JavaScript HTTP trigger function processed a request.");
 
-  const responseMessage = `Request Send : MediaUrl = ${mediaurl} , FaXto ${to}`;
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: responseMessage,
